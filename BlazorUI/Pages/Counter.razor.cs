c317c0a01960f9d4f86e9c41a9d3ef81e48b46f4
Brian Fontana <brian@codefoxtrot.com>
2022-05-07 22:52:07 -0400

    Add CounterHub and Counter page

---
+++ BlazorUI/Pages/Counter.razor.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace BlazorUI.Pages;
+
+public partial class Counter : IAsyncDisposable
+{
+    private int _currentCount = 0;
+    private HubConnection _counterHub;
+
+    [Inject]
+    public NavigationManager NavigationManager { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        _counterHub = new HubConnectionBuilder()
+            .WithUrl(NavigationManager.ToAbsoluteUri("/counterhub"))
+            .WithAutomaticReconnect()
+            .Build();
+
+        _counterHub.On<string, int>("CounterIncrement", (user, value) => CounterIncrement(user, value));
+        await _counterHub.StartAsync();
+    }
+
+    private void CounterIncrement(string user, int value)
+    {
+        _currentCount += 1;
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        if (_counterHub is not null)
+        {
+            await _counterHub.DisposeAsync();
+        }
+    }
+}
